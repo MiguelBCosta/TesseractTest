8313a7ae8758401eff0972968aad27cfcb6da172
Miguel Bertemes Costa <miguel.costa@selbetti.com.br>
2019-05-24 17:50:46 -0300

    First Commit

---
+++ TesseractRecognizer/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TesseractRecognizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArquivos = new System.Windows.Forms.Button();
+            this.lsbFiles = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbDirSaida = new System.Windows.Forms.TextBox();
+            this.btnDirSaida = new System.Windows.Forms.Button();
+            this.btnAplicaOcr = new System.Windows.Forms.Button();
+            this.btnAbrirDirSaida = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnArquivos
+            // 
+            this.btnArquivos.Location = new System.Drawing.Point(15, 74);
+            this.btnArquivos.Name = "btnArquivos";
+            this.btnArquivos.Size = new System.Drawing.Size(181, 23);
+            this.btnArquivos.TabIndex = 1;
+            this.btnArquivos.Text = "Selecionar Arquivo";
+            this.btnArquivos.UseVisualStyleBackColor = true;
+            this.btnArquivos.Click += new System.EventHandler(this.btnArquivos_Click);
+            // 
+            // lsbFiles
+            // 
+            this.lsbFiles.Enabled = false;
+            this.lsbFiles.FormattingEnabled = true;
+            this.lsbFiles.Location = new System.Drawing.Point(15, 103);
+            this.lsbFiles.Name = "lsbFiles";
+            this.lsbFiles.Size = new System.Drawing.Size(181, 147);
+            this.lsbFiles.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Diretório de Saída:";
+            // 
+            // txbDirSaida
+            // 
+            this.txbDirSaida.Enabled = false;
+            this.txbDirSaida.Location = new System.Drawing.Point(15, 48);
+            this.txbDirSaida.Name = "txbDirSaida";
+            this.txbDirSaida.Size = new System.Drawing.Size(258, 20);
+            this.txbDirSaida.TabIndex = 5;
+            // 
+            // btnDirSaida
+            // 
+            this.btnDirSaida.Location = new System.Drawing.Point(279, 48);
+            this.btnDirSaida.Name = "btnDirSaida";
+            this.btnDirSaida.Size = new System.Drawing.Size(26, 20);
+            this.btnDirSaida.TabIndex = 6;
+            this.btnDirSaida.Text = "...";
+            this.btnDirSaida.UseVisualStyleBackColor = true;
+            this.btnDirSaida.Click += new System.EventHandler(this.btnDirSaida_Click);
+            // 
+            // btnAplicaOcr
+            // 
+            this.btnAplicaOcr.Location = new System.Drawing.Point(202, 104);
+            this.btnAplicaOcr.Name = "btnAplicaOcr";
+            this.btnAplicaOcr.Size = new System.Drawing.Size(103, 25);
+            this.btnAplicaOcr.TabIndex = 7;
+            this.btnAplicaOcr.Text = "Aplicar OCR";
+            this.btnAplicaOcr.UseVisualStyleBackColor = true;
+            this.btnAplicaOcr.Click += new System.EventHandler(this.btnAplicaOcr_Click);
+            // 
+            // btnAbrirDirSaida
+            // 
+            this.btnAbrirDirSaida.Location = new System.Drawing.Point(202, 135);
+            this.btnAbrirDirSaida.Name = "btnAbrirDirSaida";
+            this.btnAbrirDirSaida.Size = new System.Drawing.Size(103, 39);
+            this.btnAbrirDirSaida.TabIndex = 8;
+            this.btnAbrirDirSaida.Text = "   Abrir Diretorio     de Saida";
+            this.btnAbrirDirSaida.UseVisualStyleBackColor = true;
+            this.btnAbrirDirSaida.Click += new System.EventHandler(this.btnAbrirDirSaida_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 261);
+            this.Controls.Add(this.btnAbrirDirSaida);
+            this.Controls.Add(this.btnAplicaOcr);
+            this.Controls.Add(this.btnDirSaida);
+            this.Controls.Add(this.txbDirSaida);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lsbFiles);
+            this.Controls.Add(this.btnArquivos);
+            this.MaximumSize = new System.Drawing.Size(328, 300);
+            this.MinimumSize = new System.Drawing.Size(328, 300);
+            this.Name = "Form1";
+            this.Text = "Tesseract Recognize";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArquivos;
+        private System.Windows.Forms.ListBox lsbFiles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbDirSaida;
+        private System.Windows.Forms.Button btnDirSaida;
+        private System.Windows.Forms.Button btnAplicaOcr;
+        private System.Windows.Forms.Button btnAbrirDirSaida;
+    }
+}
+
---
+++ TesseractRecognizer/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace TesseractRecognizer
+{
+    public partial class Form1 : Form
+    {
+        public List<string> _arquivo = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            txbDirSaida.Text = Application.StartupPath;
+        }
+
+        private void btnDirSaida_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog()
+            {
+                Description = "Seleciona o Diretório de Saída"
+            };
+            if (folder.ShowDialog() == DialogResult.OK)
+                txbDirSaida.Text = folder.SelectedPath;
+        }
+
+        private void btnArquivos_Click(object sender, EventArgs e)
+        {
+            _arquivo = new List<string>();
+            FileDialog file = new OpenFileDialog() { Multiselect = true };
+            if (file.ShowDialog() == DialogResult.OK)
+                _arquivo = file.FileNames.ToList();
+            _arquivo.ForEach(filePath => lsbFiles.Items.Add(Path.GetFileName(filePath)));
+        }
+
+        private void btnAplicaOcr_Click(object sender, EventArgs e)
+        {
+
+            string caminho = Path.Combine(Application.StartupPath, "tessdata");
+            using (TesseractEngine tesseract = new TesseractEngine(caminho, "por"))
+                foreach (string file in _arquivo)
+                    using (var render = ResultRenderer.CreatePdfRenderer(Path.Combine(txbDirSaida.Text, Path.GetFileNameWithoutExtension(file)), Path.Combine(caminho, caminho)))
+                    using (var image = Pix.LoadFromFile(file))
+                    using (render.BeginDocument(Path.GetFileNameWithoutExtension(file)))
+                        render.AddPage(tesseract.Process(image, Path.GetFileName(file)));
+        }
+
+        private void btnAbrirDirSaida_Click(object sender, EventArgs e)
+        {
+            Process.Start(txbDirSaida.Text);
+        }
+
+
+
+    }
+}
